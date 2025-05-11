0c2f454572e2f906f2d0edc65e26f647b5cf6426
runjie <939991685@qq.com>
2025-05-11 16:17:04 +0000

    exercise 5-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_144246) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_11_145438) do
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "style"
+    t.integer "brewery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "breweries", force: :cascade do |t|
     t.string "name"
     t.integer "year"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+b1 = Brewery.create name: "Koff", year: 1897
+b2 = Brewery.create name: "Malmgard", year: 2001
+b3 = Brewery.create name: "Weihenstephaner", year: 1040
+
+b1.beers.create name: "Iso 3", style: "Lager"
+b1.beers.create name: "Karhu", style: "Lager"
+b1.beers.create name: "Tuplahumala", style: "Lager"
+b2.beers.create name: "Huvila Pale Ale", style: "Pale Ale"
+b2.beers.create name: "X Porter", style: "Porter"
+b3.beers.create name: "Hefeweizen", style: "Weizen"
+b3.beers.create name: "Helles", style: "Lager"
