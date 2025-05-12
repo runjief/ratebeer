c2fb18f2b60504a0d6fd68560b7edc3261adfc7d
runjie <939991685@qq.com>
2025-05-12 10:22:57 +0000

    week3 exercise14-15

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,14 @@ class User < ApplicationRecord
   include RatingAverage
   has_secure_password
   validates :username, uniqueness: true,
-                      length: { minimum: 3, maximum: 30 }
+                       length: { minimum: 3, maximum: 30 }
   validates :password, length: { minimum: 4 },
                       format: { 
                         with: /\A(?=.*[A-Z])(?=.*\d).+\z/, 
                         message: "must contain at least one uppercase letter and one number" 
                       }
-  has_many :ratings
+
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
   has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
