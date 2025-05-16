e385e128d62c9371a3792f578a2334970542e49f
runjie <939991685@qq.com>
2025-05-16 21:32:35 +0000

    fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,10 @@
+
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
 
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+import "@popperjs/core"
+import "bootstrap"
 import "controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap" # @5.3.6
